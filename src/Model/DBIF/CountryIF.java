420dd29dc752b33b176925f2a5f8aa1217e3da84
TheKillerKid <kristian.hjelm@hotmail.com>
2021-05-12 13:58:01 +0200

    making DB and DBIF for employee

---
+++ src/Model/DBIF/CountryIF.java
@@ -0,0 +1,16 @@
+package Model.DBIF;
+
+public class CountryIF {
+
+	/*public Country getCountry(long id) {
+		return id;
+	}
+	
+	public ArrayList<Country> getCountries() {
+		
+	}
+	
+	private Country buildObject(ResultSet rs) {
+		
+	}*/
+}
