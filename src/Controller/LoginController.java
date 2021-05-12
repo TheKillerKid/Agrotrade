035919e95474595669325f75c1e217afad769efd
TheKillerKid <kristian.hjelm@hotmail.com>
2021-05-12 10:24:02 +0200

    Creating Person superclass

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,5 +1,16 @@
 package Controller;
 
+//import EmployeeController;
+
 public class LoginController {
 
+	private String email;
+	private String password;
+	//private EmployeeController employeeController = new EmployeeController();
+
+
+	public Employee login(String email, String password) {
+		this.email = new email();
+		this.password = new password();
+	}
 }
