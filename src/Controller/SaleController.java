99f8135388d50600f52a2c3e4feef3afa6e4f9f5
ondrejspok <ondrej.spok@gmail.com>
2021-05-12 13:55:14 +0200

    SaleDB SaleIF created

--- src/Controller/SaleController.java
+++ src/Controller/SaleController.java
@@ -1,5 +1,13 @@
 package Controller;
 
-public class SaleController {
+import Model.DB.SaleDB;
+import Model.Model.Order;
+import Model.Model.Sale;
 
+public class SaleController {
+	private SaleDB saleDB = new SaleDB();
+
+	public long createSale(Sale sale) {
+		return 0;
+	}
 }
