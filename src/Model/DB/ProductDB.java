6dc6beb8accdb450133bf3b7c21a73fcf4ade74a
BranikMacik <branislavmacko2@gmail.com>
2021-05-13 11:49:23 +0200

    add buildProduct

--- src/Model/DB/ProductDB.java
+++ src/Model/DB/ProductDB.java
@@ -1,8 +1,12 @@
 package Model.DB;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+
 import Model.Model.Product;
 import Model.DBIF.ProductDBIF;
 
@@ -24,6 +28,41 @@ public class ProductDB implements ProductDBIF {
 	}
 
 	private Product buildProduct(ResultSet rsProduct) {
+		return null;
+	}
+
+	
+	String sqlCreate = "INSERT INTO Product (barcode, name) VALUES (?,?)";    				//
+	@Override
+	public long createProduct(Product product) throws SQLException {
+		
+		long rowID = 0;
+		long barcode = product.getBarcode();
+		String name = product.getName();
+		//int category_id = 1;			missing method 										
+		//int unit_id
+		//int supplier_id
+		try (Connection con = DBConnection.getInstance().getConnection()) {
+			PreparedStatement preparedStmt = con.prepareStatement(sqlCreate);
+			preparedStmt.setLong(1, barcode);
+			preparedStmt.setString(2, name);
+		
+			rowID = preparedStmt.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		}
+		return rowID;
+	}
+
+	@Override
+	public void updateProduct(Product product) throws SQLException {
+		
+		
+	}
+
+	@Override
+	public ArrayList<Product> getProductList() throws SQLException {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
