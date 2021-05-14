1af32e3ef30da7f6d2dd55d10be23be0c672dea8
ondrejspok <ondrej.spok@gmail.com>
2021-05-14 10:34:26 +0200

    SaleDB and SaleIF edit

--- src/Controller/SaleController.java
+++ src/Controller/SaleController.java
@@ -1,13 +1,20 @@
 package Controller;
 
+import java.sql.SQLException;
+
 import Model.DB.SaleDB;
 import Model.Model.Order;
 import Model.Model.Sale;
 
 public class SaleController {
 	private SaleDB saleDB = new SaleDB();
-
-	public long createSale(Sale sale) {
+	
+	public long createSale(Sale sale) throws SQLException {
+		try {
+			return saleDB.createSale(sale);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return 0;
 	}
 }
