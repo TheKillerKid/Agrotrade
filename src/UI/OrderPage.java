5413dfdf6f33b662cb9734fb929b12fd7f74c314
TheKillerKid <kristian.hjelm@hotmail.com>
2021-05-18 09:32:04 +0200

    making UI

--- src/UI/OrderPage.java
+++ src/UI/OrderPage.java
@@ -8,10 +8,21 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
 
 public class OrderPage extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 	
 	public static void start() {
 		EventQueue.invokeLater(new Runnable() {
@@ -44,25 +55,145 @@ public class OrderPage extends JDialog {
 	 * Create the dialog.
 	 */
 	public OrderPage() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 640, 480);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{97, 100, 259, 88, 84, 0};
+		gbl_contentPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
+		{
+			JLabel lblNewLabel = new JLabel("Create Order");
+			lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+			GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+			gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel.gridx = 2;
+			gbc_lblNewLabel.gridy = 2;
+			contentPanel.add(lblNewLabel, gbc_lblNewLabel);
+		}
+		{
+			JLabel lblNewLabel_1 = new JLabel("New label");
+			GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+			gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+			gbc_lblNewLabel_1.gridx = 1;
+			gbc_lblNewLabel_1.gridy = 3;
+			contentPanel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		}
+		{
+			textField = new JTextField();
+			GridBagConstraints gbc_textField = new GridBagConstraints();
+			gbc_textField.insets = new Insets(0, 0, 5, 5);
+			gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField.gridx = 2;
+			gbc_textField.gridy = 3;
+			contentPanel.add(textField, gbc_textField);
+			textField.setColumns(10);
+		}
+		{
+			JLabel lblNewLabel_2 = new JLabel("New label");
+			GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+			gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
+			gbc_lblNewLabel_2.gridx = 1;
+			gbc_lblNewLabel_2.gridy = 4;
+			contentPanel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		}
+		{
+			textField_1 = new JTextField();
+			GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+			gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+			gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField_1.gridx = 2;
+			gbc_textField_1.gridy = 4;
+			contentPanel.add(textField_1, gbc_textField_1);
+			textField_1.setColumns(10);
+		}
+		{
+			JLabel lblNewLabel_3 = new JLabel("New label");
+			GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+			gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
+			gbc_lblNewLabel_3.gridx = 1;
+			gbc_lblNewLabel_3.gridy = 5;
+			contentPanel.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		}
+		{
+			textField_2 = new JTextField();
+			GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+			gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+			gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField_2.gridx = 2;
+			gbc_textField_2.gridy = 5;
+			contentPanel.add(textField_2, gbc_textField_2);
+			textField_2.setColumns(10);
+		}
+		{
+			JLabel lblNewLabel_4 = new JLabel("New label");
+			GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+			gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
+			gbc_lblNewLabel_4.gridx = 1;
+			gbc_lblNewLabel_4.gridy = 6;
+			contentPanel.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		}
+		{
+			textField_3 = new JTextField();
+			GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+			gbc_textField_3.insets = new Insets(0, 0, 5, 5);
+			gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField_3.gridx = 2;
+			gbc_textField_3.gridy = 6;
+			contentPanel.add(textField_3, gbc_textField_3);
+			textField_3.setColumns(10);
+		}
+		{
+			JLabel lblNewLabel_5 = new JLabel("New label");
+			GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+			gbc_lblNewLabel_5.insets = new Insets(0, 0, 0, 5);
+			gbc_lblNewLabel_5.anchor = GridBagConstraints.EAST;
+			gbc_lblNewLabel_5.gridx = 1;
+			gbc_lblNewLabel_5.gridy = 7;
+			contentPanel.add(lblNewLabel_5, gbc_lblNewLabel_5);
+		}
+		{
+			textField_4 = new JTextField();
+			GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+			gbc_textField_4.insets = new Insets(0, 0, 0, 5);
+			gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField_4.gridx = 2;
+			gbc_textField_4.gridy = 7;
+			contentPanel.add(textField_4, gbc_textField_4);
+			textField_4.setColumns(10);
+		}
 		{
 			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			GridBagLayout gbl_buttonPane = new GridBagLayout();
+			gbl_buttonPane.columnWidths = new int[]{81, 70, 311, 80, 84, 0};
+			gbl_buttonPane.rowHeights = new int[]{23, 0};
+			gbl_buttonPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+			gbl_buttonPane.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+			buttonPane.setLayout(gbl_buttonPane);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton btnNewButton = new JButton("Back");
+				GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+				gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+				gbc_btnNewButton.gridx = 1;
+				gbc_btnNewButton.gridy = 0;
+				buttonPane.add(btnNewButton, gbc_btnNewButton);
+				//btnNewButton.addActionListener();
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				JButton btnNewButton_1 = new JButton("Save");
+				GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+				gbc_btnNewButton_1.insets = new Insets(0, 0, 0, 5);
+				gbc_btnNewButton_1.gridx = 3;
+				gbc_btnNewButton_1.gridy = 0;
+				buttonPane.add(btnNewButton_1, gbc_btnNewButton_1);
 			}
 		}
 	}
